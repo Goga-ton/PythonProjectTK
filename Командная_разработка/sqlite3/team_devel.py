28b6c50bf6c3e0ffd74f613e990816b81efbb47c
Goga-ton <goga_ton@mail.ru>
2026-03-06 11:37:41 +0300

    Presentation on GitHub

---
+++ Командная_разработка/sqlite3/team_devel.py
@@ -0,0 +1,22 @@
+import sqlite3
+conn = sqlite3.connect('example.db')
+cur = conn.cursor()
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS users
+#         (id INTEGER PRIMARY KEY,
+#         name TEXT NOT NULL,
+#         age INTEGER NOT NULL)
+#     ''')
+# conn.commit()
+#
+# cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Алексей", 35))
+# cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Алена", 22))
+# conn.commit()
+
+cur.execute("SELECT * FROM users")
+# print(cur.fetchall())
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+conn.close()
